598a6c42400c5c2f66cdc2d7b03788d8c65916d3
Alex Ley <a.p.ley@outlook.com>
2018-08-06 23:57:52 +0200

    added css, and new and existing sections

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = angular.module('CDS-config-app', []);
-	app.controller('configurator', function($scope) {
+	app.controller('configurator', ['$scope', function($scope) {
 		$scope.todos = [ {text:'Learn AngularJS', done:true},
 								{text:'build a single page AngularJS app', done:false}];
 		$scope.addTodo = function() {
@@ -9,7 +9,28 @@ var app = angular.module('CDS-config-app', []);
 			}
 		};
 
-	$scope.showExisting = function() {};
+		$scope.new_values = {
+								'Instrument Clients':1,
+								'Thermo Fisher Instruments':1,
+								'3rd Party GC Instruments':0,
+								'3rd Party LC Instruments':0,
+								'Total Instruments':1,
+								'Data Clients':2,
+							};
+
+		$scope.existing_values = {
+								'Instrument Clients':2,
+								'Thermo Fisher Instruments':1,
+								'3rd Party GC Instruments':0,
+								'3rd Party LC Instruments':0,
+								'Total Instruments':1,
+								'Data Clients':2,
+							};
+
+
+
+		$scope.showExisting = function() {};
+
 		$scope.removeCompleted = function() {
 			for (var i = $scope.todos.length - 1; i >= 0; i--){
 			    if( $scope.todos[i].done === true){
@@ -18,4 +39,20 @@ var app = angular.module('CDS-config-app', []);
 			    }
 			}
 		};
-	});
+
+		$scope.new_installation = function() {
+			alert("new");
+		};
+
+		$scope.existing_installation = function() {
+			alert("existing");
+		};
+
+		$scope.update_new = function() {
+			alert("update new");
+		};
+
+		$scope.update_existing = function() {
+			alert("update exist");
+		};
+	}]);
